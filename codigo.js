551718a27fb40411a97eaf1c727ae93f7caeec31
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-20 20:36:42 -0500

    Ejercicio 18

--- codigo.js
+++ codigo.js
@@ -143,5 +143,15 @@ feed.pop();
 
 console.log(feed);
 
+//ejercicio 18
+
+let invitados = ["Carlos", "Beatriz", "David", "Ana"];
+
+invitados.sort();
+invitados.splice(1, 1);
+
+console.log(invitados);
+
+
 
 
