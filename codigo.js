26dc02e6d601c01451cd49d209731e8670bc9abe
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-20 20:07:22 -0500

    Ejercicio 13

--- codigo.js
+++ codigo.js
@@ -98,3 +98,11 @@ commits.pop();
 commits.push("v1.2-corregido");
 
 console.log(commits);
+
+//ejercicio 13
+
+let receta = ["Paso 2: Mezclar ingredientes", "Paso 3: Hornear"];
+receta.unshift("Paso 1: Pre-calentar horno");
+receta.push("Paso 4: Servir");
+
+console.log(receta);
