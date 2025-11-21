2a82c070b19d2f0710305299262ffcf00e872b8c
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-20 20:38:42 -0500

    Ejercicio 20

--- codigo.js
+++ codigo.js
@@ -160,6 +160,17 @@ tablero.fill("Casilla Vacía", 1, 5);
 
 console.log(tablero);
 
+//ejercicio 20
+
+let codigos = ["A-10", "C-30", "B-20", "D-40"];
+
+codigos.sort();
+codigos.splice(2, 1);
+codigos.unshift("X-99");
+codigos.reverse();
+
+console.log(codigos);
+
 
 
 
