ca32ad99e13be52242e6da41d2376be0ee7ed487
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-20 20:08:59 -0500

    Ejercicio 15

--- codigo.js
+++ codigo.js
@@ -116,3 +116,12 @@ palabras.splice(0, 1, "tres");
 
 console.log(palabras);
 
+//ejercicio 15
+
+let equipo = ["Ana (Líder)", "Juan (Dev)", "Maria (Diseñadora)"];
+
+equipo.splice(1, 1);
+equipo.push("Pedro (Dev)", "Luisa (Dev)");
+
+console.log(equipo);
+
