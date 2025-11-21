2fa0737640bd666a61b9ae1d11355923fd606a21
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-20 20:08:10 -0500

    Ejercicio 14

--- codigo.js
+++ codigo.js
@@ -106,3 +106,13 @@ receta.unshift("Paso 1: Pre-calentar horno");
 receta.push("Paso 4: Servir");
 
 console.log(receta);
+
+//ejercicio 14
+
+let palabras = ["uno", "dos", "cuatro"];
+
+palabras.reverse();
+palabras.splice(0, 1, "tres");
+
+console.log(palabras);
+
