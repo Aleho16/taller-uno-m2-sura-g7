4df2848c698de266b8ee6aa9ee4fa66fc0826bd1
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-20 19:52:43 -0500

    Ejercicio 4

--- codigo.js
+++ codigo.js
@@ -26,3 +26,11 @@ historial.pop();
 
 console.log(historial);
 
+//ejercicio 4
+let colaImpresion = ["doc1.pdf", "doc2.png", "doc3.jpg"];
+
+colaImpresion.shift();
+colaImpresion.push("doc4.txt");
+colaImpresion.shift();
+
+console.log(colaImpresion);
